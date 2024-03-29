c8982d05b7282130801eafdb3bac6200b991cf0d
Lynross <mauriciobarva@gmail.com>
2024-03-29 18:44:17 -0500

    feat: update to handleSquareClick

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,14 +1,40 @@
 import { useState } from "react";
-import { mountBoard } from "../utils/board";
+import { mountBoard, mountPlayerScore } from "../utils/board";
 import SquareButton from "./SquareButton";
 
 export default function TicTacToe({ boardSize = 3 }) {
   const [board, setBoard] = useState(mountBoard(boardSize));
+  const [playerXScore, setPlayerXScore] = useState(mountPlayerScore(boardSize));
+  const [playerOScore, setPlayerOScore] = useState(mountPlayerScore(boardSize));
   const [turn, setTurn] = useState("X");
-
+  const [winner, setWinner] = useState(null);
   function handleSquareClick(targetRowIndex, targetColIndex) {
+    // Esta validación podría pasarse a una función para que sea más mantenible
     if (!!board[targetRowIndex][targetColIndex]) return;
+    // Se puede crear una función que se encargue de actualizar los scores y retorne si hay ganador
+    // Update scores
+    const isXTurn = turn === "X";
+    const newScore = isXTurn ? [...playerXScore] : [...playerOScore];
+    // Update row score
+    newScore[targetRowIndex] += 1;
+    // Update col score
+    newScore[targetColIndex + boardSize] += 1;
+    // Check and update diag
+    if (!(targetRowIndex - targetColIndex)) {
+      newScore[newScore.length - 2] += 1;
+    }
+    // Check and update anti-diag
+    if ((targetRowIndex + targetColIndex) === (boardSize - 1)) {
+      newScore[newScore.length - 1] += 1;
+    }
 
+    // Check if there is a winner
+    if (newScore.includes(boardSize)) {
+      setWinner(turn);
+      return;
+    }
+    // Se puede crear una función que se encargue de devolver el nuevo tablero
+    // Create the board to render
     const newBoard = board.map((row, rowIndex) => {
       if (rowIndex === targetRowIndex) {
         const newNow = [...row];
@@ -19,8 +45,31 @@ export default function TicTacToe({ boardSize = 3 }) {
       return row;
     });
 
+    if (isXTurn) setPlayerXScore(newScore);
+    else setPlayerOScore(newScore);
     setBoard(newBoard);
-    setTurn(turn === "X" ? "O" : "X");
+    setTurn(isXTurn ? "O" : "X");
+  }
+
+  function handlePlayAgain() {
+    setBoard(mountBoard(boardSize));
+    setPlayerXScore(mountPlayerScore(boardSize));
+    setPlayerOScore(mountPlayerScore(boardSize));
+    setWinner(null);
+    setTurn('X');
+  }
+
+  if (winner) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col d-flex justify-content-center flex-column">
+            <h2 className="text-center py-5">¡Felicidades jugador {winner} has ganado!</h2>
+            <button className="btn btn-primary mx-auto" onClick={handlePlayAgain}>Jugar otra vez</button>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return (
--- src/utils/board.js
+++ src/utils/board.js
@@ -1,3 +1,7 @@
 export function mountBoard(size) {
   return new Array(size).fill(new Array(size).fill(""));
 }
+
+export function mountPlayerScore(boardSize) {
+  return new Array((boardSize * 2) + 2).fill(0);
+}
